aa9d78f87d9b82a672955c2ce44de65d7e0e37b3
Ji Won (Jiwi) Chong <42596420+JiwiChong@users.noreply.github.com>
2024-11-04 16:43:39 -0700

    Add files via upload

--- eval.py
+++ eval.py
@@ -5,9 +5,10 @@ import pickle
 import numpy as np
 import torch.nn as nn
 from torch import optim
-from utils import CustomImageDataset, calculate_metric, init_weights, DeepCrackDataset
+from utils import CustomImageDataset, init_weights, DeepCrackDataset
 from eval_metrics import f1_score, iou_score
 from model import UNet_FCN, LMM_Net
+from sklearn.metrics import confusion_matrix, precision_score, recall_score
 from efficientnet import EfficientCrackNet
 from torchvision import datasets, transforms, models
 from torch.utils.data.sampler import SubsetRandomSampler
@@ -24,19 +25,19 @@ warnings.filterwarnings("ignore")
 
 # Testing model
 def eval(args, test_dataloaders):
+    
     if args.model_name == 'UNet':
-        model = UNet_FCN(args = args).to(device)
+        model = UNet_FCN(args = args, scaler=2).to(device)
         model.apply(init_weights)
     elif args.model_name == 'LMM_Net':
         model = LMM_Net().to(device)
     elif args.model_name == 'EfficientCrackNet':
         model = EfficientCrackNet().to(device)
 
-    model.load_state_dict(torch.load(f'./saved_models/{args.model_name}/best_model_num_{args.run_num}.pt'), map_location=device)['model_state_dict']
+    model.load_state_dict(torch.load(f'./saved_models/{args.model_name}/best_model_num_{args.run_num}.pt')['model_state_dict'])
 
     model.eval()
-    all_y, all_y_hat = [], []
-    f1_scores, iou_scores = 0.0, 0.0
+    f1_scores, recall_scores, precision_scores, iou_scores = 0.0, 0.0, 0.0, 0.0
     num_batch = 0.0
     # metrics: IoU, BF score, F1 score, precision, recall 
 
@@ -45,23 +46,34 @@ def eval(args, test_dataloaders):
         with torch.no_grad():
             # Add batch to GPU
             output_mask= model(input_img)
-            
-            # y = labels.detach().cpu().numpy().tolist()
-            # y_pred = outputs.detach().cpu().numpy().tolist()
-            # all_y.extend(y)
-            # all_y_hat.extend(y_pred)
-            f1_s = f1_score(mask, output_mask)
-            iou = iou_score(mask, output_mask)
+            output_mask[output_mask > 0.5] = 1.
+            output_mask[output_mask < 0.5] = 0.
+
+            f1_s = f1_score(mask.cpu().numpy().flatten(), output_mask.cpu().numpy().flatten())
+            recall = recall_score(mask.cpu().numpy().flatten(), output_mask.cpu().numpy().flatten())
+            precision = precision_score(mask.cpu().numpy().flatten(), output_mask.cpu().numpy().flatten())
+            cm = confusion_matrix(mask.cpu().numpy().flatten(), output_mask.cpu().numpy().flatten(), labels=[0, 1])
+
+            intersection = np.diag(cm)
+            ground_truth_set = cm.sum(axis=1)
+            predicted_set = cm.sum(axis=0)
+            union = ground_truth_set + predicted_set - intersection
+            iou = intersection / union.astype(np.float32)
+            iou_scores += np.mean(iou)
+
             f1_scores += f1_s
-            iou_scores += iou
+            recall_scores += recall
+            precision_scores += precision
             num_batch += 1.0
 
     test_f1_score = (f1_scores/num_batch)
+    test_recall_score = (recall_scores/num_batch)
+    test_precision_score = (precision_scores/num_batch)
     test_miou_score = (iou_scores/num_batch)
 
-    # final_all_y_pred = np.argmax(all_y_hat, axis=1)
-    # metric = calculate_metric(all_y, final_all_y_pred)
     print(f'Test F1 Score is: {round(test_f1_score, 2)}')
+    print(f'Test Recall Score is: {round(test_recall_score, 2)}')
+    print(f'Test Precision Score is: {round(test_precision_score, 2)}')
     print(f'Test mIoU Score is: {round(test_miou_score, 2)}')
 
 if __name__ == '__main__':
@@ -87,8 +99,12 @@ if __name__ == '__main__':
 
     if args.data_name == 'deepcrack':
         test_dataset = DeepCrackDataset(args, data_part='test')
-        test_dataloaders = DataLoader(test_dataset, batch_size=8, num_workers=10)
+        test_dataloaders = DataLoader(test_dataset, batch_size=8, num_workers=5)
 
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     eval(args, test_dataloaders)
+
+
+
+# python eval.py --data_dir C:/Users/jwkor/Documents/UNM/crack_segmentation/dataset/DeepCrack/ --model_name UNet --data_name deepcrack --run_num 1
